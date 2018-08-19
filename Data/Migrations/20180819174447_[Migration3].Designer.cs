cf30aff3d9f0059af53997e839b3c919b9c71452
Fryann <fryann14@hotmail.com>
2018-08-19 15:57:35 -0400

    Adding Model Categoria and Cursos

--- Data/Migrations/20180819174447_[Migration3].Designer.cs
+++ Data/Migrations/20180819174447_[Migration3].Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace SistemaAC.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180819174447_[Migration3]")]
+    partial class Migration3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Models/Categoria.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaAC.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int CatagoriaID { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public Boolean Estado { get; set; } = true;
+        public ICollection<Curso> Cursos { get; set; }
+    }
+}
